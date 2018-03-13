97f3f3ee73ac1be4423236b1731a1ddf7340752b
andrewcywang <husky.cywang@gmail.com>
2018-03-13 14:19:59 +0800

    更名

---
+++ hello.py
@@ -0,0 +1,7 @@
+def main():
+    if __name__ == '__main__':
+        print('This program is run directly.')
+    elif __name__ == 'hello':
+        print('This program is used as a module.')
+
+main()  #呼叫上面定義的函數
